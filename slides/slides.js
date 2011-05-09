e560c8f1944fb274f33f3fe44f7f3b7be73bdfe7
Paul Irish <paul.irish@gmail.com>
2011-05-08 17:00:56 -0700

    transforms slid.e

--- slides/slides.js
+++ slides/slides.js
@@ -9,7 +9,7 @@
   URL: http://code.google.com/p/io-2011-slides/
 */
 
-var PERMANENT_URL_PREFIX = 'http://io-2011-slides.googlecode.com/svn/trunk/';
+var PERMANENT_URL_PREFIX = './';
 
 var SLIDE_CLASSES = ['far-past', 'past', 'current', 'next', 'far-next'];
 
